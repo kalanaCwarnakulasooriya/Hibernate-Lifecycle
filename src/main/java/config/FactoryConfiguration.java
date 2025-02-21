5996adaf729dd41b741410ce058048ad987a8d97
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-02-21 10:44:43 +0530

    xml and config file create

---
+++ src/main/java/config/FactoryConfiguration.java
@@ -0,0 +1,29 @@
+package config;
+
+//import entity.Customer;
+//import entity.Customer;
+//import entity.IDCard;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+
+    private SessionFactory sessionFactory; //session factory ekn session create krl dena object eka.
+    private static FactoryConfiguration factoryConfiguration;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration().configure(); //Eliye thiyana xml file eka thniym hoygena read krl link krnne meya.hibernate boot krnw/snap krnw
+//        configuration.addAnnotatedClass(Customer.class);
+//        configuration.addAnnotatedClass(IDCard.class);
+        sessionFactory  = configuration.buildSessionFactory();
+    }
+    public static FactoryConfiguration getInstance() {
+        return  (factoryConfiguration == null) ?
+                 factoryConfiguration = new FactoryConfiguration():
+                 factoryConfiguration;
+    }
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
